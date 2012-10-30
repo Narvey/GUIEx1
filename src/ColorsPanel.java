88f75774edfb4f27f1633d7417ff5f52fbece782
N Harvey <nthnhrvy@gmail.com>
2012-10-30 09:01:17 -0600

    First Commit

---
+++ src/ColorsPanel.java
@@ -0,0 +1,24 @@
+import java.awt.LayoutManager;
+import java.awt.Panel;
+
+
+public class ColorsPanel extends Panel {
+
+	public ColorsPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ColorsPanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/NamesPanel.java
@@ -0,0 +1,24 @@
+import java.awt.LayoutManager;
+import java.awt.Panel;
+
+
+public class NamesPanel extends Panel {
+
+	public NamesPanel() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NamesPanel(LayoutManager layout) {
+		super(layout);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
